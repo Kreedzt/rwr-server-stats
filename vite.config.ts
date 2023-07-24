288ea40b2b93dbab2ba831bfea3b0a5f812b6cbb
Kreedzt <zhaozisong1@live.com>
2023-07-24 22:44:51 +0800

    :sparkles: add project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://rwr.runningwithrifles.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      }
-    }
-  }
-})
+export default defineConfig((config) => {
+  return {
+    plugins: [react()],
+    base: "/",
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://rwr.runningwithrifles.com",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
+});
